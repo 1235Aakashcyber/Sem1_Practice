3b2ff95022ede9cabb4de3a976d25d12572a2884
Aakash Goswami <Aakash172004@gmail.com>
2024-10-25 14:25:39 +0530

    Update transpose.c

--- CA2/transpose.c
+++ CA2/transpose.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-int r,c;
+int r,c,temp;
 printf("Enter No. of rows : ");
 scanf("%d",&r);
 printf("Enter No. of columns : ");
@@ -11,9 +11,22 @@ for(int i=0; i<r; i++){
         printf("Enter Element A[%d][%d] : ");
         scanf("%d",&A[i][j]);
     }
+printf("\n");
 }
-for(int i=0; i<c; i++){
-    for(int j=0; j<r; j++){
+printf("Matrix is \n");
+
+for(int i=0; i<r; i++){
+    for(int j=0; j<c; j++){
+        printf("%d",A[i][j]);
+}
+}
+printf("\n");
+printf("Transpose is \n");
+for(int i=0; i<r; i++){
+    for(int j=0; j<c; j++){
+        temp = A[i][j];
+        A[i][j] = A[j][i];
+        A[j][i] = temp;
         printf("%d",A[i][j]);
     }
 }
