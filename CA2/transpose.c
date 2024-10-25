fe21f077e0a18514188c6e07113afa53d72a296e
Aakash Goswami <Aakash172004@gmail.com>
2024-10-25 18:43:25 +0530

    Update transpose.c

--- CA2/transpose.c
+++ CA2/transpose.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 int main(){
-int r,c,temp;
-printf("Enter No. of rows : ");
-scanf("%d",&r);
-printf("Enter No. of columns : ");
-scanf("%d",&c);
-int A[r][c];
-for(int i=0; i<r; i++){
-    for(int j=0; j<c; j++){
-        printf("Enter Element A[%d][%d] : ");
+int temp;
+int A[3][3];
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        printf("Enter Element A[%d][%d] : ",i,j);
         scanf("%d",&A[i][j]);
     }
-printf("\n");
 }
-printf("Matrix is \n");
-
-for(int i=0; i<r; i++){
-    for(int j=0; j<c; j++){
-        printf("%d",A[i][j]);
-}
-}
-printf("\n");
-printf("Transpose is \n");
-for(int i=0; i<r; i++){
-    for(int j=0; j<c; j++){
-        temp = A[i][j];
-        A[i][j] = A[j][i];
-        A[j][i] = temp;
-        printf("%d",A[i][j]);
+printf("________________________________________________________________\n Real Matrix is\n");
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        printf("%d \t",A[i][j]);
     }
+    printf(" \n");
+}
+
+for(int i=0; i<3; i++){
+    for(int j=0; j<i; j++){
+       temp = A[i][j];
+       A[i][j] = A[j][i]; 
+       A[j][i] = temp; 
+    }
+}
+printf("________________________________________________________________\n Transpose Matrix is\n");
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        printf("%d \t",A[i][j]);
+    }
+    printf(" \n");
 }
 return 0;    
 }
