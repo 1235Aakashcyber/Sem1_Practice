84c295e4383f62eb218a334f0b6618bb8dd696d4
Aakash Goswami <Aakash172004@gmail.com>
2024-10-25 07:46:32 +0530

    Update pattern11.c

--- Week_6/pattern11.c
+++ Week_6/pattern11.c
@@ -19,10 +19,7 @@ for(int i =1; i<=n; i++){
     for(int k=0; k<=n-i-1; k++){
         printf("\t");
     }
-    for(int j=1; j<=i; j++){
-        printf("* \t");
-    }
-    for(int m=1; m<i; m++){
+    for(int j=1; j<=(2*i-1); j++){
         printf("* \t");
     }
     printf("\n");
