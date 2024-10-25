729f9e252a715aac93c7eb076627a5883474ceae
Aakash Goswami <Aakash172004@gmail.com>
2024-10-25 18:07:35 +0530

    Update array_sort.c

--- CA2/array_sort.c
+++ CA2/array_sort.c
@@ -1,25 +1,29 @@
-// To arrange an array in ascending order.
+// This program arrange the input numbers in ascending order.
 
 #include<stdio.h>
 int main(){
-    int size;
+    int size,temp;
     printf("Enter the size of an array :");
     scanf("%d",&size);
     int A[size];
 
     for(int i=0; i<size; i++){
-        printf("Enter element %d : ",i);
+        printf("Enter number %d : ",i+1);
         scanf("%d",&A[i]);
     }
-    int max=A[0];
-    for(int j=1; j<size; j++){
-        if(max<A[j]){
-            max = A[j];
+
+    for(int i=0; i<size-1; i++){
+        for(int j=0; j<size; j++){
+        if(A[j+1]<A[j]){
+            temp = A[j+1];
+            A[j+1] = A[j];
+            A[j] = temp;
         }
-        else {
-            continue;
         }
     }
-    printf("%d",max);
+
+    for(int i=0; i<size; i++){
+    printf("%d \t",A[i]);
+    }
     return 0;
 }
